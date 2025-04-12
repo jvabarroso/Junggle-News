3c94b64f5888207bf9faec141f50e92106a987b9
João Vitor Alves Barroso <jvabarroso@gmail.com>
2025-04-12 18:29:55 -0300

    Norte-Sul_Version

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask
 from controllers import routes
 
 app = Flask(__name__, template_folder='views')
 routes.init_app(app)
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000, debug=True)
+    app.run(host='localhost', port=4000, debug=True)
--- controllers/routes.py
+++ controllers/routes.py
@@ -1,10 +1,53 @@
 from flask import render_template, request 
 
+comentarios = [{'Comentário':''}]
+
+feedbacks = [{'Nome':'Igor',
+             'DataNasc':'',
+             'Sexo': '',
+             'Email': '',
+             'Comentário':''}]
+
+noticias = [{'Título': '',
+             'Autor':'',
+             'Categoria':'',
+             'Descrição':'',
+             'Notícia':''}]
+
 def init_app(app):
     @app.route('/')
     def home():
-        return render_template('index.html')
+        return render_template('index.html', method=['GET', 'POST'])
     
-    @app.route('/noticia', methods=['GET', 'POST'])
+    @app.route('/noticia')
     def noticia():
-        return render_template('noticia.html')
+        dadosComentarios = comentarios[0]
+        if request.method == 'POST':
+            if request.form.get('comentario'):
+                comentarios.append({'Comentário': request.form.get('comentario')})
+        return render_template('noticia.html', dadosComentarios=dadosComentarios, comentarios=comentarios)
+    
+    @app.route('/feedback', methods=['GET', 'POST'])
+    def feedback():
+        dadosFeedbacks = feedbacks[0]
+        if request.method == 'POST':
+            if request.form.get('nome') and request.form.get('nascimento') and request.form.get('sexo') and request.form.get('email') and request.form.get('comentario'):
+                feedbacks.append({'Nome': request.form.get('nome'),
+                                 'dataNasc': request.form.get('nascimento'),
+                                 'Sexo': request.form.get('sexo'),
+                                 'Email': request.form.get('email'),
+                                 'Comentário': request.form.get('comentario')})
+        return render_template('feedback.html', dadosFeedbacks=dadosFeedbacks, feedback=feedbacks)
+    
+    @app.route('/addNoticia', methods=['GET', 'POST'])
+    def addNoticia():
+        dadosNoticias = noticias[0]
+        if request.form == 'POST':
+            if request.form.get('titulo') and request.form.get('autor') and request.form.get('categoria') and request.form.get('descricao') and request.form.get('noticia'):
+                noticias.append({'Título': request.form.get('titulo'),
+                                 'Autor': request.form.get('Autor'),
+                                 'Categoria': request.form.get('Categoria'),
+                                 'Descrição': request.form.get('descricao'),
+                                 'Título': request.form.get('noticia'),
+                                 })
+        return render_template('addNoticia.html', dadosNoticias=dadosNoticias, noticias=noticias)
