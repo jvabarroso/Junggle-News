74994b20340b716ee2773fc795bfc9dba7cae00f
João Vitor Alves Barroso <jvabarroso@gmail.com>
2025-04-13 16:30:49 -0300

    Junggle-News LONS_Version

--- controllers/routes.py
+++ controllers/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, request 
 
-comentarios = [{'Comentário':''}]
+comentarios = [{'Nome':'',
+                'Comentário':''}]
 
 feedbacks = [{'Nome':'Igor',
              'DataNasc':'',
@@ -19,25 +20,41 @@ def init_app(app):
     def home():
         return render_template('index.html', method=['GET', 'POST'])
     
-    @app.route('/abelhas')
-    def abelhas():
-        return render_template('abelhas.html')
-    
-    @app.route('/tigres')
-    def tigres():
-        return render_template('tigres.html')
-    
-    @app.route('/lobo-terrivel')
-    def lobo_terrivel():
-        return render_template('noticia.html')
-    
-    @app.route('/noticia')
+    @app.route('/baleias', methods=['GET', 'POST'])
     def noticia():
         dadosComentarios = comentarios[0]
         if request.method == 'POST':
-            if request.form.get('comentario'):
-                comentarios.append({'Comentário': request.form.get('comentario')})
-        return render_template('noticia.html', dadosComentarios=dadosComentarios, comentarios=comentarios)
+            if request.form.get('comentario') and request.form.get('nome'):
+                comentarios.append({'Nome': request.form.get('nome'),
+                                    'Comentário': request.form.get('comentario')})
+        return render_template('baleias.html', dadosComentarios=dadosComentarios, comentarios=comentarios)
+    
+    @app.route('/abelhas', methods=['GET', 'POST'])
+    def abelhas():
+        dadosComentarios = comentarios[0]
+        if request.method == 'POST':
+            if request.form.get('comentario') and request.form.get('nome'):
+                comentarios.append({'Nome': request.form.get('nome'),
+                                    'Comentário': request.form.get('comentario')})
+        return render_template('abelhas.html', dadosComentarios=dadosComentarios, comentarios=comentarios)
+    
+    @app.route('/tigres', methods=['GET', 'POST'])
+    def tigres():
+        dadosComentarios = comentarios[0]
+        if request.method == 'POST':
+           if request.form.get('comentario') and request.form.get('nome'):
+                comentarios.append({'Nome': request.form.get('nome'),
+                                    'Comentário': request.form.get('comentario')})
+        return render_template('tigres.html', dadosComentarios=dadosComentarios, comentarios=comentarios)
+    
+    @app.route('/lobo-terrivel', methods=['GET', 'POST'])
+    def lobo_terrivel():
+        dadosComentarios = comentarios[0]
+        if request.method == 'POST':
+           if request.form.get('comentario') and request.form.get('nome'):
+                comentarios.append({'Nome': request.form.get('nome'),
+                                    'Comentário': request.form.get('comentario')})
+        return render_template('lobo-terrivel.html', dadosComentarios=dadosComentarios, comentarios=comentarios)
     
     @app.route('/feedback', methods=['GET', 'POST'])
     def feedback():
@@ -45,21 +62,21 @@ def init_app(app):
         if request.method == 'POST':
             if request.form.get('nome') and request.form.get('nascimento') and request.form.get('sexo') and request.form.get('email') and request.form.get('comentario'):
                 feedbacks.append({'Nome': request.form.get('nome'),
-                                 'dataNasc': request.form.get('nascimento'),
+                                 'DataNasc': request.form.get('nascimento'),
                                  'Sexo': request.form.get('sexo'),
                                  'Email': request.form.get('email'),
                                  'Comentário': request.form.get('comentario')})
-        return render_template('feedback.html', dadosFeedbacks=dadosFeedbacks, feedback=feedbacks)
+        return render_template('feedback.html', dadosFeedbacks=dadosFeedbacks, feedbacks=feedbacks)
     
     @app.route('/addNoticia', methods=['GET', 'POST'])
     def addNoticia():
         dadosNoticias = noticias[0]
-        if request.form == 'POST':
+        if request.method == 'POST':
             if request.form.get('titulo') and request.form.get('autor') and request.form.get('categoria') and request.form.get('descricao') and request.form.get('noticia'):
                 noticias.append({'Título': request.form.get('titulo'),
-                                 'Autor': request.form.get('Autor'),
+                                 'Autor': request.form.get('autor'),
                                  'Categoria': request.form.get('Categoria'),
                                  'Descrição': request.form.get('descricao'),
-                                 'Título': request.form.get('noticia'),
+                                 'Notícia': request.form.get('noticia'),
                                  })
         return render_template('addNoticia.html', dadosNoticias=dadosNoticias, noticias=noticias)
